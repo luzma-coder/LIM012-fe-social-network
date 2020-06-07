cc3ba054cb945332503003040305b1772d72d90b
IsabelaSanchez <natyst_26@hotmail.com>
2020-06-07 05:45:14 -0500

    Fourth Commit: Editing/Adding css of each view
    *We added the directory img which contains the logo, drawings, facebook and gmail icon
    *We edited the html of each view
    *We edited the css
    *We created classes and ids to use in the css
    *We called our font with google fonts
    *We added our icons with fontawesome

--- src/view/home.js
+++ src/view/home.js
@@ -1,11 +1,16 @@
 export default () => {
   const viewHome = `
   <div>
-  <h1>INICIA SESION</h1>
-  <input type="text" placeholder="Correo electronico">
-  <input type="text" placeholder="Contraseña">
-  <a href="#/">Ingresar</a>
-  <h1>No tienes cuenta <a href="#/register">Registrate</a></h1>
+  <img class="logo" src="img/logo.png">
+  <img id="dance" src="img/dance.png">
+  <p class='subtitle'>INICIA SESIÓN</p>
+  <div><i class="far fa-user"></i><input type="text" placeholder="Correo electronico"></div>
+  <div><i class="fas fa-lock"></i><input type="text" placeholder="Contraseña"><div>
+  <button class='principal-button'><a class='links-on-buttons' href="#/">INGRESAR</a></button>
+  <p class='lil-text'>O ingresa con...</p>
+  <a class='links-on-buttons' href="#/"><img id="face" src="img/facebook.png"></a>
+  <a class='links-on-buttons' href="#/"><img id="gmail" src="img/gmail.png"></a>
+  <p class='lil-text'>¿No tienes una cuenta?</p><a id='just-link'href="#/register"> REGISTRATE</a>
   </div>`;
   const divElemt = document.createElement('div');
 
