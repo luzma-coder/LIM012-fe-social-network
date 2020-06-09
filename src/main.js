844fbd35acd3b38e60ee4e18b2fa3d771be5afab
IsabelaSanchez <natyst_26@hotmail.com>
2020-06-09 11:31:59 -0500

    My new changes

--- src/main.js
+++ src/main.js
@@ -18,3 +18,11 @@ const firebaseConfig = {
 };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    window.location.hash = '/wall';
+  } else {
+    window.location.hash = '/home';
+  }
+});
--- src/view/home.js
+++ src/view/home.js
@@ -8,7 +8,7 @@ export default () => {
   <p class='subtitle'>INICIA SESIÓN</p>
   <div><i class="far fa-user"></i><input id ="email" type="text" placeholder="Correo electronico"></div>
   <div><i class="fas fa-lock"></i><input id ="pass" type="password" placeholder="Contraseña"><div>
-  <button id = "btn-login" class='principal-button'><a class='links-on-buttons' href="#/">INGRESAR</a></button>
+  <button id = "btn-login" class='principal-button'><a class='links-on-buttons' href="#/wall">INGRESAR</a></button>
   <p class='lil-text'>O ingresa con...</p>
   <a class='links-on-buttons' href="#/"><img id="face" src="img/facebook.png"></a>
   <a class='links-on-buttons' href="#/"><img id="gmail" src="img/gmail.png"></a>
