683f7f924075ab3bf29123167ad408e3714c657e
luzma-coder <luz.marina.acdr@gmail.com>
2020-06-09 21:26:20 -0500

    css: desktop, style y index. clase view-register

--- src/view/home.js
+++ src/view/home.js
@@ -2,19 +2,17 @@ import { logIn } from '../model/firebase.js';
 
 export default () => {
   const viewHome = `
-  <div>
   <img class="logo" src="img/logo.png">
   <img id="dance" src="img/dance.png">
-  <p class='subtitle'>INICIA SESIÓN</p>
-  <div><i class="far fa-user"></i><input id ="email" type="text" placeholder="Correo electronico"></div>
-  <div><i class="fas fa-lock"></i><input id ="pass" type="password" placeholder="Contraseña"><div>
-  <button id = "btn-login" class='principal-button'><a class='links-on-buttons' href="#/">INGRESAR</a></button>
-  <p class='lil-text'>O ingresa con...</p>
-  <a class='links-on-buttons' href="#/"><img id="face" src="img/facebook.png"></a>
-  <a class='links-on-buttons' href="#/"><img id="gmail" src="img/gmail.png"></a>
-  <p class='lil-text'>¿No tienes una cuenta?</p><a id='just-link'href="#/register"> REGISTRATE</a>
-  </div>`;
+<p class='subtitle'>INICIA SESIÓN</p>
+<div><i class="far fa-user"></i><input id ="email" type="text" placeholder="Correo electronico"></div>
+<div><i class="fas fa-lock"></i><input id ="pass" type="password" placeholder="Contraseña"></div>
+<button id = "btn-login" class='principal-button'><a class='links-on-buttons' href="#/">INGRESAR</a></button>
+<p class='lil-text'>O ingresa con...</p>
+<a class='links-on-buttons' href="#/"><img id="face" src="img/facebook.png"></a><a class='links-on-buttons' href="#/"><img id="gmail" src="img/gmail.png"></a>
+<p class='lil-text'>¿No tienes una cuenta?</p><a id='just-link'href="#/register"> REGISTRATE</a>`;
   const divElemt = document.createElement('div');
+  divElemt.classList.add('view-register');
   divElemt.innerHTML = viewHome;
 
   const btnLogIn = divElemt.querySelector('#btn-login');
