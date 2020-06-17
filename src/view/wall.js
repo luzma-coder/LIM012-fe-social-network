92dee2e1973891cac5eab0bceabead171ec64dc2
IsabelaSanchez <natyst_26@hotmail.com>
2020-06-17 07:16:13 -0500

    mine

--- src/view/wall.js
+++ src/view/wall.js
@@ -1,7 +1,9 @@
 export default () => {
   const viewWal = `
     <h2>Holi soy el muro de moves</h2>
-    <button id = "btn-logout" class='principal-button'><a class='links-on-buttons' href="#/">CERRAR SESIÓN</a></button>
+    <textarea rows="4" cols="50" placeholder="¿Qué quieres compartir?" id="postArea"></textarea>
+    <button id="btnSharePost"> Compartir</button>
+    <button id ="btn-logout" class='principal-button'><a class='links-on-buttons' href="#/">CERRAR SESIÓN</a></button>
     `;
   const divElemt = document.createElement('div');
   divElemt.innerHTML = viewWal;
