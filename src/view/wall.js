3d16b9ef6c4b20a35508d48e48ff2596299b3f78
IsabelaSanchez <natyst_26@hotmail.com>
2020-06-22 21:08:40 -0500

    Delete option
    *We create a delete button

--- src/view/wall.js
+++ src/view/wall.js
@@ -37,8 +37,7 @@ export default () => {
       </section>
   </section>
     `;
-//añadir este estilo clase overflow para crear un scroll
-
+  // añadir este estilo clase overflow para crear un scroll
   const divElemt = document.createElement('div');
   divElemt.classList.add('view-wall');
   divElemt.innerHTML = viewWall;
@@ -46,7 +45,7 @@ export default () => {
   getPosts((objArray) => {
     // allPost(objArray);
     postSection.innerHTML = '';
-    objArray.forEach(element => {
+    objArray.forEach((element) => {
       postSection.appendChild(allPost(element));
     });
     console.log(objArray);
